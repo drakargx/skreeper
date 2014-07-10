fcf0fbc996d039b28d2aa5d227e26148e971e141
drakargx <drakargx@openmailbox.org>
2014-07-10 15:53:59 -0500

    updated main

--- skreeper/main.cpp
+++ skreeper/main.cpp
@@ -1,11 +1,5 @@
-#include <SFML/Graphics.hpp>
 #include <chrono>
 #include "Game.h"
-
-using FrameTime = float;
-//const float ftStep{ 1.f }, ftSlice{ 1.f };
-
-sf::CircleShape shape(100.f);
 
 int main()
 {
